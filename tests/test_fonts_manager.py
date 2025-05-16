107d5d6609a351b291012072c2e253ba70aa3747
ctrl-alt-op <ctrl.alt.op@gmail.com>
2025-05-16 15:41:38 -0600

    Skip for non-windows machines

--- tests/test_fonts_manager.py
+++ tests/test_fonts_manager.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from unittest.mock import patch
+import pytest
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../nebulatk"))
@@ -16,6 +17,7 @@ def test_create_font():
     assert fonts_manager.Font("Arial").font == ("Arial", 10)
 
 
+@pytest.mark.skipif(sys.platform != "win32", reason="windll only on Windows")
 @patch("ctypes.windll.gdi32.AddFontResourceExW")
 def test_load_font(mock_windll):
     if fonts_manager.ctypes_available:
