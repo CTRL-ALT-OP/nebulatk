4a651225e3d112e7a747c5b40e3095b4b582b234
ctrl-alt-op <ctrl.alt.op@gmail.com>
2025-05-16 16:56:40 -0600

    Prep for unix support

--- tests/test_fonts_manager.py
+++ tests/test_fonts_manager.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 import pytest
 
 sys.path.insert(
@@ -20,5 +20,15 @@ def test_create_font():
 @pytest.mark.skipif(sys.platform != "win32", reason="windll only on Windows")
 @patch("ctypes.windll.gdi32.AddFontResourceExW")
 def test_load_font(mock_windll):
-    if fonts_manager.ctypes_available:
-        assert fonts_manager.loadfont("test.ttf")
+    assert fonts_manager.ctypes_available
+    assert fonts_manager.loadfont("test.ttf")
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="windll only on Windows")
+@patch("libfc.FcConfigAppFontAddFile")
+@patch("libfc.FcConfigBuildFonts")
+def test_load_font_linux(mock_libfc, mock_libfc_dir):
+    assert not fonts_manager.ctypes_available
+    assert fonts_manager.loadfont("test.ttf")
+    mock_libfc.assert_called_once_with(ANY, "test.ttf")
+    mock_libfc_dir.assert_called_once_with(ANY)
